2c9b775f59a4dde2c885c9aad0dc816fd5fa391d
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2026-02-12 21:38:29 +0500

    added setup fixture

--- app/src/test/java/Base/BaseTest.java
+++ app/src/test/java/Base/BaseTest.java
@@ -1,11 +1,18 @@
 package Base;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
     
+    @BeforeAll
+    static void setup(){
+        Configuration.headless = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
     @AfterEach
     void tearDown(){
         closeWebDriver();
